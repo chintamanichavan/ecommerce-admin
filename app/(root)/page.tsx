30ed1dbdef60af657cdbbcc95016f851e4009f30
Chintamani Chavan <chintamanichavan@Chintamanis-MBP.cable.rcn.com>
2023-07-09 22:35:43 -0500

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import { Button } from '@/components/ui/button'
+
+
+export default function Home() {
+  return (
+    <div className='p-4'>
+        <Button size="default">Click Me</Button>
+    </div>
+  )
+}
